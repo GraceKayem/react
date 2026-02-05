1de3b6eb31c527d60bce8478f09b827a9e58cb21
GraceKayem <11557@holbertonstudents.com>
2026-02-05 20:22:11 +1100

    mapping

--- 298-mapping-components/src/components/App.jsx
+++ 298-mapping-components/src/components/App.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+function createCard(contacts) {
+  return <Card 
+  id={contacts.id} 
+  key={contacts.id}
+    name={contacts.name}
+    img={contacts.imgURL}
+    phone={contacts.phone}
+    email={contacts.email}
+  />
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
 
-      <Card
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,7 +37,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
