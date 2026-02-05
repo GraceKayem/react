c06e391759c112f4403e7bb0ea5c7b67979ac493
GraceKayem <11557@holbertonstudents.com>
2026-02-05 17:28:36 +1100

    recat devtools

--- 297-react-devtools/src/components/Card.jsx
+++ 297-react-devtools/src/components/Card.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import Avatar from "./Avatar";
-
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <Avatar />
+        <img className="circle-img" src={props.img} alt="avatar_img" />
       </div>
       <div className="bottom">
         <p className="info">{props.tel}</p>
